3d63b312f1853b5015dd93d5b1434d7fce3f1a98
Anthony C <37058209+acathers@users.noreply.github.com>
2020-06-24 17:33:31 -1000

    apply base layout

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,60 +1,31 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+    <v-app>
+        <v-app-bar app color="blue-grey" dark>
+            <router-link to="/" tag="button">DiscordServerTracker</router-link>
+            <v-spacer/>
+            <router-link to="/about" tag="button">About</router-link>
+        </v-app-bar>
+        <v-main>
+            <v-container>
+                <router-view/>
+            </v-container>
+        </v-main>
+        <v-footer app color="blue-grey" class="white--text">
+            <span>DiscordServerTracker</span>
+            <v-spacer></v-spacer>
+            <span>&copy; 2020</span>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
 
-export default {
-  name: 'App',
+    export default {
+        name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+        components: {},
 
-  data: () => ({
-    //
-  }),
-};
+        data: () => ({
+        }),
+    };
 </script>
